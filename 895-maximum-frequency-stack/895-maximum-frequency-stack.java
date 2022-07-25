6c2c30e3e56ca14f6ef407a2f24553ad9fd29318
harshagg21521 <80303169+harshagg21521@users.noreply.github.com>
2022-07-26 05:05:03 +0530

    Time: 280 ms (5.10%), Space: 86.5 MB (9.05%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -0,0 +1,48 @@
+class FreqStack {
+        HashMap<Integer,Integer> map;
+        HashMap<Integer,Stack<Integer>> freq;
+        int maxfreq=0;
+
+    public FreqStack() {
+            map=new HashMap<>();
+            freq=new HashMap<>();
+    }
+    
+    public void push(int val) {
+        map.put(val,map.getOrDefault(val,0)+1);
+        if(freq.containsKey(map.get(val))){
+            Stack<Integer> st=freq.get(map.get(val));
+            st.push(val);
+            freq.put(map.get(val),st);
+        }
+        else{
+            Stack<Integer> si=new Stack<>();
+            si.push(val);
+            freq.put(map.get(val),si);
+        }
+    }
+    
+    public int pop() {
+        Stack<Integer> sl=freq.get(freq.size());
+        int ans=sl.pop();
+        freq.put(freq.size(),sl);
+        if(freq.get(freq.size()).size()==0){
+            freq.remove(freq.size());
+        }
+        if(map.get(ans)>1){
+            map.put(ans,map.get(ans)-1);
+        }
+        else{
+            map.remove(ans);
+        }
+        System.out.println(ans);
+        return ans;
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
