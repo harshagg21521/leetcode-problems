d2be8635faea7a06ee4f4002acb40892070d1be2
harshagg21521 <80303169+harshagg21521@users.noreply.github.com>
2022-09-18 15:56:22 +0530

    Time: 16 ms (44.48%), Space: 55.2 MB (59.83%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,54 +1,23 @@
 class Solution {
-    public class Pair implements Comparable <Pair>{
-        int start;
-        int end;
-        
-        public int compareTo(Pair o){
-            if(this.start!=o.start){
-                return this.start-o.start;
-            }
-            else{
-                return this.end-o.end;
-            }
-        }
-    }
     public int[][] merge(int[][] intervals) {
-        Pair[] arr=new Pair[intervals.length];
-        for(int i=0;i<arr.length;i++){
-            arr[i]=new Pair();
-            arr[i].start=intervals[i][0];
-            arr[i].end=intervals[i][1];
-
-        }
-        Arrays.sort(arr);
-        Stack<Pair> st=new Stack<>();
-        st.push(arr[0]);
-        for(int i=1;i<arr.length;i++){
-            Pair top=st.peek();
-            if(arr[i].start>top.end){
-                st.push(arr[i]);
+        Arrays.sort(intervals,(a,b)->{
+            return a[0]-b[0];
+        });
+        ArrayList<int[]> list=new ArrayList<>();
+        for(int[] interval:intervals){
+            if(list.size()==0){
+                list.add(interval);
             }
             else{
-                top.end=Math.max(top.end,arr[i].end);
+                int[] top=list.get(list.size()-1);
+                if(interval[0]>top[1]){
+                    list.add(interval);
+                }
+                else{
+                    top[1]=Math.max(top[1],interval[1]);
+                }
             }
-          
         }
-        Stack<Pair> rev=new Stack();
-        while(st.size()>0){
-            rev.push(st.pop());
-        }
-                   
-        List<Pair> list= new ArrayList<>();
-            while(rev.size()>0){
-                list.add(rev.pop());
-            }
-        int[][] ans=new int[list.size()][2];
-        int i=0;
-        for(Pair pair:list){
-            ans[i][0]=pair.start;
-            ans[i][1]=pair.end;
-            i++;
-        }
-        return ans;
+        return list.toArray(new int[list.size()][2]);
     }
 }
