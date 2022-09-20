981dc2620451e0378d4c621aaa93404c8bb0a71c
harshagg21521 <80303169+harshagg21521@users.noreply.github.com>
2022-09-20 12:43:28 +0530

    Time: 1878 ms (5.00%), Space: 153.4 MB (33.93%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] nge=new int[nums.length];
+        int[] ans=new int[nums.length-k+1];
+        Stack<Integer> st=new Stack<>();
+        st.push(nums.length-1);
+        nge[nums.length-1]=nums.length;
+        for(int i=nums.length-2;i>=0;i--){
+            int val=nums[i];
+            while(st.size()>0&&val>=nums[st.peek()]){
+                st.pop();
+            }
+            if(st.size()==0){
+                nge[i]=nums.length;
+            }
+            else{
+                nge[i]=st.peek();
+            }
+            st.push(i);
+        }
+        int i=0,j=0;
+        while(i<=nums.length-k){
+            if(nge[j]<k+i){
+                j=nge[j];
+            }
+            else{
+                ans[i]=nums[j];
+                i++;
+                j=i;
+            }
+        }
+        return ans;
+    }
+}
